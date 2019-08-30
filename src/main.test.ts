4b3bc4ba3b3223a6e56a8a73ad006ccc621ee0b1
Chris Wendt <chrismwendt@gmail.com>
2019-08-30 17:09:41 -0600

    generate at test time

--- src/main.test.ts
+++ src/main.test.ts
@@ -2,19 +2,34 @@ import { index } from './index'
 import { Edge, Vertex } from 'lsif-protocol'
 import _ from 'lodash'
 import * as path from 'path'
+import * as cp from 'child_process'
 
-test('does not emit items with duplicate IDs', async () => {
+async function generateAndIndex(example: string): Promise<(Edge | Vertex)[]> {
     const output: (Edge | Vertex)[] = []
 
+    cp.execFileSync('./generate-csv', ['$CXX -c *.cpp'], {
+        env: {
+            ABSROOTDIR: path.resolve(`examples/${example}/root`),
+            ABSOUTDIR: path.resolve(`examples/${example}/output`),
+            CLEAN: 'true',
+        },
+    })
+
     await index({
-        csvFileGlob: 'examples/five/output/*.csv',
-        root: 'examples/five/root',
+        csvFileGlob: `examples/${example}/output/*.csv`,
+        root: `examples/${example}/root`,
         emit: item =>
             new Promise(resolve => {
                 output.push(item)
                 resolve()
             }),
     })
+
+    return output
+}
+
+test('does not emit items with duplicate IDs', async () => {
+    const output = await generateAndIndex('five')
 
     const setsOfDupes = _(output)
         .groupBy(item => item.id)
@@ -39,17 +54,7 @@ test('does not emit items with duplicate IDs', async () => {
 })
 
 test('output', async () => {
-    const output: string[] = []
-
-    await index({
-        csvFileGlob: 'examples/five/output/*.csv',
-        root: 'examples/five/root',
-        emit: item =>
-            new Promise(resolve => {
-                output.push(JSON.stringify(item))
-                resolve()
-            }),
-    })
+    const output = (await generateAndIndex('five')).map(v => JSON.stringify(v))
 
     expect(output.join('\n')).toMatchSnapshot()
 })
