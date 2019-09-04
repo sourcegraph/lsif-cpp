5ac8dcc705e3bfd444245c85e9271e5bf3c046a9
Chris Wendt <chrismwendt@gmail.com>
2019-09-04 12:32:21 -0600

    printf

--- examples/cross-app/root/app.cpp
+++ examples/cross-app/root/app.cpp
@@ -1,7 +1,8 @@
 #include "lib.h"
+#include <stdio.h>
 
 int main() {
     // TODO support printf (no defloc on the ref)
     // printf("hello, %s\n", uppercase("world"));
-    uppercase("world");
+    printf("hello, %s\n", uppercase("world"));
 }
